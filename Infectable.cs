1284548b981cceaa9783500da1858c4d50beb439
Sir-Cactus <35261379+Sir-Cactus@users.noreply.github.com>
2018-01-27 02:52:06 +0000

    updated infection files

--- Infectable.cs
+++ Infectable.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Infectable : MonoBehaviour {
 
-	private int infectionlevel;
+	private float infectionlevel; // if this reaches 45 then game over (starts as 45 seconds of exposure then decreases)
 	private bool infecting;
 
-	FixedUpdate(){ // called 60 times a second
-		if(infecting){
-			infectionlevel++;
+
+	void FixedUpdate(){ // called 60 times a second(ish)
+		if(infecting){ // adds the time of exposure since the last frame
+			Debug.Log(infectionlevel);
+			infectionlevel += Time.deltaTime *getInfectionMulti();
 		}
+
 		// checks if the infected person is dead due to the 	
-		if(infectionlevel > 2700){
+		if(infectionlevel > 45.0f){
+			setInfecting(false);
+			Debug.Log("end of game?");
 			Application.Quit(); // change to restart level later
 		}
 	}
@@ -24,10 +29,16 @@ public class Infectable : MonoBehaviour {
 	}
 
 	public void disinfect(){
+		infectionlevel = 0;
 		setInfecting(false);
 	}
 
 	private void setInfecting(bool newVal){
 		infecting = newVal;
 	}
+
+	float getInfectionMulti(){
+		//call the lift object for this value
+	}
+
 }
