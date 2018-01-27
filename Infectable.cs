aebde7016860d3e3a0e1591d804dd37aef734660
Sir-Cactus <35261379+Sir-Cactus@users.noreply.github.com>
2018-01-27 01:10:03 +0000

    basic collision (might be buggy)

---
+++ Infectable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infectable : MonoBehaviour {
+
+	private int infectionlevel;
+	private bool infecting;
+
+	FixedUpdate(){ // called 60 times a second
+		if(infecting){
+			infectionlevel++;
+		}
+		// checks if the infected person is dead due to the 	
+		if(infectionlevel > 2700){
+			Application.Quit(); // change to restart level later
+		}
+	}
+
+
+	public void infect(){
+		Debug.Log("infection has begun");
+		setInfecting(true);
+	}
+
+	public void disinfect(){
+		setInfecting(false);
+	}
+
+	private void setInfecting(bool newVal){
+		infecting = newVal;
+	}
+}
---
+++ infection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class infection : MonoBehaviour {
+
+	void OnCollisionEnter (Collision col) {
+		if(col.gameObject.tag == "Infectable") {
+			col.gameObject.GetComponent<Infectable>().infect();
+		}
+	}
+
+	void OnCollisionExit (Collision col) {
+		if(col.gameObject.tag == "Infectable") {
+			 col.gameObject.GetComponent<Infectable>().disinfect();
+		}
+	}
+}
