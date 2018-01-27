418db225bfe6074ed701fe0b6439b1eac526f2f3
BrendanSavem <35852085+BrendanSavem@users.noreply.github.com>
2018-01-27 12:51:49 +0000

    Updated Doorscript and Camera Control
    DoorScript is updated with audio cues and sounds . SmoothMouseLook is a borrowed script for 1st person camera movement. Also attached are elevator audio cues (in WAV format) done by Thomas.

--- DoorScript.cs
+++ DoorScript.cs
@@ -7,20 +7,47 @@ public class DoorScript : MonoBehaviour {
     public bool doorID;     //True if Left, False if Right
     private float doorOpenPos;  //Door's Open Position. On opposing sides of origin
     private float doorClosePos; //Door's Closed Position. On opposing sides of the origin
-    public float doorCurrPos;   //Door's Current Position, range between Open and Closed Positions
+    private float doorMoveDis;  //Range of movement on the doors, range between open and closed positions
+    public float doorCurrPos;   //Door's Current Position, on the range of MoveDis
     public bool doorState;  //True if Open, False if Closed
+    private float doorOpenSpeed;    //How fast the doors open
+    private float doorCloseSpeed;   //How fast the doors close
+
+    private bool audioCue;  //Designates which door manages the audio, to prevent double looping of audio cues. Left door by default
+    public AudioClip doorOpening;
+    private float openingTime;
+    public AudioClip doorClosing;
+    private float closingTime; 
+    private AudioSource source;
+    private float volLowRange = 0.5f;
+    private float volHighRange = 1.0f;
 
 	// Use this for initialization
 	void Start ()
     {
-		if (doorID == true)
+        source = GetComponent<AudioSource>();
+        
+        //Identify left and right doors
+		if (doorID == true) //Left Door
         {
             doorOpenPos = 6;
             doorClosePos = 2.75f;
-        } else {
+            
+            
+        } else {    //Right Door
             doorOpenPos = -6;
             doorClosePos = -2.75f;
         }
+        doorMoveDis = doorOpenPos - doorClosePos;
+
+        //Get Sound Lengths, print them to log
+        openingTime = doorOpening.length;
+        closingTime = doorClosing.length;
+        print(openingTime + " and " + closingTime);
+
+        //Calculate doorMoveSpeed for opening and closing doors
+        doorOpenSpeed = doorMoveDis / openingTime;
+        doorCloseSpeed = doorMoveDis / closingTime;
 	}
 	
 	// Update is called once per frame
@@ -28,29 +55,40 @@ public class DoorScript : MonoBehaviour {
     {
         if (Input.GetButtonDown("Jump"))
         {
-            openDoor();
+            doorState = true;
+            source.PlayOneShot(doorOpening, 1); //Play Opening Sound
         }
         if (Input.GetButtonDown("Fire3"))
         {
+            doorState = false;
+            source.PlayOneShot(doorClosing, 1); //Play Closing Sound
+        }
+
+        if(doorState == true)
+        {
+            openDoor();
+        } else
+        {
             closeDoor();
         }
+
         transform.localPosition = new Vector3 (7, 4, doorCurrPos);
     }
 
     void openDoor()
     {
-        if (doorCurrPos != doorOpenPos)
+        if (Mathf.Abs(doorCurrPos) <= Mathf.Abs(doorOpenPos))
         {
-            doorCurrPos = doorOpenPos;
+            doorCurrPos += doorOpenSpeed;
             doorState = true;
         }
     }
 
     void closeDoor()
     {
-        if (doorCurrPos != doorClosePos)
+        if (Mathf.Abs(doorCurrPos) >= Mathf.Abs(doorClosePos))
         {
-            doorCurrPos = doorClosePos;
+            doorCurrPos -= doorCloseSpeed;
             doorState = false;
         }
     }
